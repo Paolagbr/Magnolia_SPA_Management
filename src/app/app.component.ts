4be77e9251454ff7d0bf91d0dee6dcfdbbef09f6
Paolagbr <paolagbr555@gmail.com>
2026-03-27 18:32:36 -0600

    cambioTitulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,5 +21,5 @@ import { MenuComponent } from "./menu/menu.component";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniProyectoANGULAR';
+  title = 'Magnolia SPA';
 }
